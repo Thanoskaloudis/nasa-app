6a597e53dd4dbe1c7bb31da585ec0bc3063e89e2
Thanos Kaloudis <38354865+Thanoskaloudis@users.noreply.github.com>
2021-04-18 19:46:20 +0100

    implement a build process with webpack

---
+++ dist/server/index.js
@@ -0,0 +1,155 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/server/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/server/index.js":
+/*!*****************************!*\
+  !*** ./src/server/index.js ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! dotenv */ \"dotenv\").config();\n\nconst express = __webpack_require__(/*! express */ \"express\");\n\nconst bodyParser = __webpack_require__(/*! body-parser */ \"body-parser\");\n\nconst fetch = __webpack_require__(/*! node-fetch */ \"node-fetch\");\n\nconst path = __webpack_require__(/*! path */ \"path\");\n\nconst app = express();\nconst port = 3000;\nconst rover_endpoint = 'https://api.nasa.gov/mars-photos/api/v1';\napp.use(bodyParser.urlencoded({\n  extended: false\n}));\napp.use(bodyParser.json());\napp.use('/', express.static(path.join(__dirname, '../public'))); // API calls\n\napp.get('/rovers/:rover_name', async (req, res) => {\n  const {\n    rover_name\n  } = req.params;\n\n  try {\n    let rover = await fetch(`\n            ${rover_endpoint}/manifests/${req.params.rover_name}?api_key=${process.env.API_KEY}\n        `).then(res => res.json());\n    res.send(rover);\n  } catch (err) {\n    console.log('error: ', err);\n  }\n});\napp.get('/rover_photos/:rover_name', async (req, res) => {\n  try {\n    const {\n      rover_name\n    } = req.params;\n    let rover_photos = await fetch(`${rover_endpoint}/rovers/${rover_name}/latest_photos?api_key=${process.env.API_KEY}\n        `).then(res => res.json());\n    res.send(rover_photos);\n  } catch (err) {\n    console.log('error: ', err);\n  }\n});\napp.listen(port, () => console.log(`Example app listening on port ${port}!`));\n\n//# sourceURL=webpack:///./src/server/index.js?");
+
+/***/ }),
+
+/***/ "body-parser":
+/*!******************************!*\
+  !*** external "body-parser" ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"body-parser\");\n\n//# sourceURL=webpack:///external_%22body-parser%22?");
+
+/***/ }),
+
+/***/ "dotenv":
+/*!*************************!*\
+  !*** external "dotenv" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"dotenv\");\n\n//# sourceURL=webpack:///external_%22dotenv%22?");
+
+/***/ }),
+
+/***/ "express":
+/*!**************************!*\
+  !*** external "express" ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"express\");\n\n//# sourceURL=webpack:///external_%22express%22?");
+
+/***/ }),
+
+/***/ "node-fetch":
+/*!*****************************!*\
+  !*** external "node-fetch" ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"node-fetch\");\n\n//# sourceURL=webpack:///external_%22node-fetch%22?");
+
+/***/ }),
+
+/***/ "path":
+/*!***********************!*\
+  !*** external "path" ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"path\");\n\n//# sourceURL=webpack:///external_%22path%22?");
+
+/***/ })
+
+/******/ });
--- src/public/client.js
+++ src/public/client.js
@@ -1,11 +1,28 @@
+import { set } from 'immutable'
+import './assets/stylesheets/resets.css'
+import './assets/stylesheets/index.css'
+import img from './assets/images/image.jpg'
+
 let store = {
-    user: { name: "Student" },
+    user: { name: 'Student' },
     apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    roverNames: [],
+    rovers: {},
+    selectedRover: '',
+    photos: {},
 }
 
 // add our markup to the page
 const root = document.getElementById('root')
+
+const formatDate = (date) => {
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const dateArr = date.split('-')
+    const year = dateArr[0]
+    const month = months[Number(dateArr[1])-1]
+    const day = dateArr[2]
+    return `${month} ${day} ${year}`
+}
 
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
@@ -16,30 +33,24 @@ const render = async (root, state) => {
     root.innerHTML = App(state)
 }
 
-
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+    let { rovers, roverNames, selectedRover, photos } = state
 
     return `
-        <header></header>
+        <header>
+            <div class="banner">
+                <img class="banner-img" src=${img} />
+                <h1 class="banner-text">Explore the Mars Rovers</h1>
+            </div>
+        </header>
         <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
         </main>
-        <footer></footer>
+        <footer>
+            <h6>
+                This page was made possible by the <a href="https://api.nasa.gov/">NASA API</a>.
+            </h6>
+        </footer>
     `
 }
 
@@ -48,47 +59,31 @@ window.addEventListener('load', () => {
     render(root, store)
 })
 
-// ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
+
+const RoverData = (rovers, selectedRover, photos) => {
+    const rover = Object.keys(rovers).find(key => key === selectedRover)
+
+    if (!rover) {
+        getRoverData(selectedRover)
     }
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+    const roverToDisplay = rovers[selectedRover];
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
+    if (roverToDisplay) {
+        return (
+            `
+                <section>
+                    <p><b>Launched:</b> ${formatDate(roverToDisplay.launch_date)}</p>
+                    <p><b>Landed:</b> ${formatDate(roverToDisplay.landing_date)}</p>
+                    <p><b>Status:</b> ${roverToDisplay.status.toUpperCase()}</p>
+                </section>
+                    
+                ${RoverPhotos(roverToDisplay.name, roverToDisplay.max_date, photos)}
+            `
+        )
+    } 
+    return `<div> Loading Data... </div>`
 }
 
 // ------------------------------------------------------  API CALLS
@@ -100,6 +95,30 @@ const getImageOfTheDay = (state) => {
     fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
+}
 
-    return data
+const getRoverData = (rover_name) => {
+    fetch(`http://localhost:3000/rovers/${rover_name}`)
+        .then(res => res.json())
+        .then(({ photo_manifest }) => updateStore(store, 
+            {
+                rovers: set(store.rovers, rover_name, {
+                    ...store.rovers[rover_name],
+                    ...photo_manifest
+                })
+            },
+        ))
+}
+
+const getLatestRoverPhotos = (rover_name) => {
+    fetch(`http://localhost:3000/rover_photos/${rover_name}`)
+        .then(res => res.json())
+        .then(({ latest_photos }) => {
+            updateStore(store, {
+                photos: {
+                    ...store.photos,
+                    [rover_name]: [...latest_photos],
+                }
+            }
+        )})
 }
